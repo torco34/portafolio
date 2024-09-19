a7d9649d59cfcd65d118997d7e704f20a75f7e11
torco34 <torcoromaarias@gmail.com>
2024-09-19 10:43:49 -0500

    Refactorizando  JS

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-const img1 = document.getElementById("img1");
-const img2 = document.getElementById("img2");
-const img3 = document.getElementById("img3");
-const img4 = document.getElementById("img4");
-const img5 = document.getElementById("img5");
+// const img1 = document.getElementById("img1");
+// const img2 = document.getElementById("img2");
+// const img3 = document.getElementById("img3");
+// const img4 = document.getElementById("img4");
+// const img5 = document.getElementById("img5");
 
+// document.getElementsByClassName
 
 const modal1 = document.getElementById("modal1");
 const modal2 = document.getElementById("modal2");
@@ -12,25 +13,25 @@ const modal4 = document.getElementById("modal4");
 const modal5 = document.getElementById("modal5");
 
 // Mostrar modal al hacer clic en la imagen
-img1.onclick = () => {
-    modal1.style.display = "block";
-}
+// img1.onclick = () => {
+//     modal1.style.display = "block";
+// }
 
-img2.onclick = function () {
-    modal2.style.display = "block";
-}
+// img2.onclick = () => {
+//     modal2.style.display = "block";
+// }
 
-img3.onclick = function () {
-    modal3.style.display = "block";
-}
+// img3.onclick = function () {
+//     modal3.style.display = "block";
+// }
 
-img4.onclick = function () {
-    modal4.style.display = "block";
-}
+// img4.onclick = function () {
+//     modal4.style.display = "block";
+// }
 
-img5.onclick = function () {
-    modal5.style.display = "block";
-}
+// img5.onclick = function () {
+//     modal5.style.display = "block";
+// }
 
 // Cerrar modal al hacer clic en la "x"
 function closeModal(modalId) {
@@ -60,3 +61,15 @@ window.onclick = function (event) {
         event.target.style.display = "none";
     }
 }
+
+const imgProject = document.getElementsByClassName("gallery-item");
+const galleryItemsArray = Array.from(document.querySelectorAll('.gallery-item'));
+
+console.log(galleryItemsArray)
+galleryItemsArray.forEach(function (item) {
+    console.log(item)
+    item.addEventListener('click', function () {
+        const modalId = this.getAttribute('onclick').match(/'([^']+)'/)[1]; // Obtener el ID del modal desde el onclick
+        openModal(modalId);
+    });
+});
