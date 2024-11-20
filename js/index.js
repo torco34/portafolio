b91e60579c4f579f0c3a3efd9b74db621a8b1378
torco34 <torcoromaarias@gmail.com>
2024-11-20 07:44:53 -0500

    Funcionando el modal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,44 @@
+
+
+// SESIÓN DE MODAL 
+
+
+// Función para abrir modal
+const openModal = (modalId) => {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+// Función para cerrar modal
+const closeModal = (modalId) => {
+    const modal = document.getElementById(modalId);
+
+    modal.style.display = "none";
+}
+
+// Cerrar modal al hacer clic fuera del contenido
+window.onclick = function (event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+}
+
+// Convertir la galería en un array y asignar eventos de clic
+const imgProjectArray = Array.from(document.querySelectorAll('.gallery-item'));
+
+imgProjectArray.forEach(function (item) {
+    item.addEventListener('click', function () {
+        const modalId = item.getAttribute('data-modal'); // Obtener el ID del modal desde 'data-modal'
+
+        openModal(modalId);
+    });
+});
+
+
+
+// SECCIÓN DE LINK ESTILOS
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const navLinks = document.querySelectorAll('.nav-link');
     const sections = document.querySelectorAll('section'); // Asegúrate de que cada sección tenga un `id` único.
