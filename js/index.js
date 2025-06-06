4071b49ff43f4371e522aa7f9eb55606f3738e12
torco34 <torcoromaarias@gmail.com>
2025-06-06 10:08:31 -0500

    diseno 6/06/25

--- js/index.js
+++ js/index.js
@@ -89,3 +89,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.nav-link');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+
+      navLinks.forEach(function (link) {
+          link.addEventListener('click', function () {
+              // Solo colapsa si está visible (abierto)
+              const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+              if (bsCollapse) {
+                  bsCollapse.hide();
+              }
+          });
+      });
+  });
